8ffb78304055c94421bef41c97f1f9e1145d9b45
dudaskobende <dudaskobende@gmail.com>
2021-12-07 11:41:36 +0100

    Add: fill karmas database

--- data_connection.py
+++ data_connection.py
@@ -1,11 +1,8 @@
 import csv
+from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
 
 import database_common
-
-
-def open_csv_file_into_dict(file_path):
-    with open(file_path, mode='r') as file:
-        return [dictionary for dictionary in csv.DictReader(file)]
 
 
 def open_csv_file(file_path):
@@ -13,42 +10,24 @@ def open_csv_file(file_path):
         return [line.split(';') for line in file.read().splitlines()]
 
 
-def create_profiles_dictionary(profiles_list):
-
-    def get_categories(categories_list):
-        category_list = []
-        for category in categories_list:
-            if 'ideal' in category:
-                category = category.split(' ', maxsplit=1)
-                category_list += category
-            else:
-                category_list.append(category)
-        return category_list
-
-    categories = get_categories(profiles_list[0])
-
-    profiles = []
-    for profile in profiles_list[1:]:
-        profile += profile[-1].split(',')
-        profile.pop(2)
-        profiles.append({categories[number]: profile[number] for number, category in enumerate(categories)})
-
-    return profiles
+def create_data_dictionary(data_list):
+    categories = data_list[0]
+    return [{categories[number]: detail[number] for number, category in enumerate(categories)} for detail in
+            data_list[1:]]
 
 
-def get_karma(karmas_list):
-    action_types = karmas_list[0]
-    karmas = []
-    for karma in karmas_list[1:]:
-        karmas.append({action_types[number]: karma[number] for number, action_type in enumerate(action_types)})
-    return karmas
+@database_common.connection_handler
+def drop_table(cursor, table_name):
+    cursor.execute(sql.SQL("""
+        DROP TABLE IF EXISTS {table}
+    """).format(table=sql.Identifier(table_name)))
 
 
 @database_common.connection_handler
 def create_karmas_table(cursor):
     query = """
         CREATE TABLE IF NOT EXISTS karmas(
-            id INTEGER PRIMARY KEY,
+            id SERIAL PRIMARY KEY,
             action_type TEXT,
             action TEXT
         )
@@ -60,27 +39,40 @@ def create_karmas_table(cursor):
 def create_profiles_table(cursor):
     query = """
         CREATE TABLE IF NOT EXISTS profiles(
-            id INTEGER PRIMARY KEY,
+            id SERIAL PRIMARY KEY,
             name TEXT,
             actions VARCHAR,
-            ideal TEXT,
-            present_categories TEXT
+            ideal_present_categories TEXT
         )
     """
     cursor.execute(query)
 
 
+@database_common.connection_handler
+def import_data_to_karmas_table(cursor, karmas):
+    for karma in karmas:
+        cursor.execute(sql.SQL("""
+            INSERT INTO karmas(action_type, action) 
+            VALUES({action_type}, {action}) 
+        """).format(
+            action_type=sql.Literal(karma['action type']),
+            action=sql.Literal(karma['action'])
+                                    ))
+
+
 def create_database():
+    drop_table('profiles')
+    create_profiles_table()
+    drop_table('karmas')
+    create_karmas_table()
     profiles_data = open_csv_file(file_path='data/profiles.csv')
-    profiles = create_profiles_dictionary(profiles_data)
+    profiles = create_data_dictionary(profiles_data)
     karmas_data = open_csv_file(file_path='data/karma.csv')
-    karmas = get_karma(karmas_data)
+    karmas = create_data_dictionary(karmas_data)
+    import_data_to_karmas_table(karmas)
     print(profiles)
     print(karmas)
-    create_profiles_table()
-    create_karmas_table()
 
 
 if __name__ == "__main__":
     create_database()
-
